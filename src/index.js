9b37f9577b78b13728624517651fdbe5db4f918a
Camilovelag <camilo.vela.galindo@gmail.com>
2022-09-27 14:14:10 -0500

    add tasks using index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 import _ from 'lodash';
 import './style.css';
 
+const taskList = document.querySelector('.task-list');
+
+const taskArr = [
+  {
+    index: 1,
+    description: 'Task 1 Lorem, ipsum dolor sit amet consectetur adipisicing elit.',
+    completed: false,
+  },
+  {
+    index: 2,
+    description: 'Task 2 Lorem, ipsum dolor sit amet consectetur adipisicing elit.',
+    completed: false,
+  },
+  {
+    index: 3,
+    description: 'Task 3 Lorem, ipsum dolor sit amet consectetur adipisicing elit.',
+    completed: false,
+  },
+  {
+    index: 4,
+    description: 'Task 4 Lorem, ipsum dolor sit amet consectetur adipisicing elit.',
+    completed: false,
+  },
+  {
+    index: 5,
+    description: 'Task 5 Lorem, ipsum dolor sit amet consectetur adipisicing elit.',
+    completed: false,
+  },
+];
+
+taskList.innerHTML = taskArr.map((task) => `<li id="task-${task.index}" class="task-item b-bottom box">
+  <input type="checkbox" class="checkbox">
+  <span class="task-text">${task.description}</span>
+  <button class="delete">X</button>
+</li>`).join('');
+
