ca3e3838e4e8008869f45cdef22f7413c42175a0
Camilovelag <camilo.vela.galindo@gmail.com>
2022-09-28 08:53:34 -0500

    Add remove feature

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,7 +1,7 @@
 export default function displayTasks(e) {
 
   const taskList = document.querySelector('.task-list');
-  const parsedArr = JSON.parse(localStorage.getItem('taskArr')) || [];
+  let parsedArr = JSON.parse(localStorage.getItem('taskArr')) || [];
 
   taskList.innerHTML = '';
   parsedArr.map((task) => {
@@ -9,7 +9,17 @@ export default function displayTasks(e) {
     taskItem.classList.add('task-item', 'b-bottom', 'box');
     taskItem.innerHTML = `<input type="checkbox" class="checkbox">
     <span class="task-text full">${task.description}</span>
-    <button class="delete btn">X</button>`;
+    <button type="button" class="delete btn">X</button>`;
     taskList.appendChild(taskItem);
+
+    const deleteBtn = taskItem.querySelector('.delete');
+    deleteBtn.addEventListener('click', () => {
+      let temp = parsedArr.filter((item) => item !== task);
+      parsedArr = temp;
+      localStorage.setItem('taskArr', JSON.stringify(parsedArr));
+    });
+
+    // fix how to display the new task array
+    return taskList;
   }); 
 }
