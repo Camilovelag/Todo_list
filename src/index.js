9ed08d3f3d592d0a573f4e8d7af81c016d28631b
Camilovelag <camilo.vela.galindo@gmail.com>
2022-09-28 17:38:50 -0500

    fix linter errors

--- src/index.js
+++ src/index.js
@@ -3,34 +3,17 @@ import './style.css';
 
 import displayTasks from './modules/display.js';
 import addTask from './modules/add.js';
-import Task from './modules/task.js';
 
 const taskList = document.querySelector('.task-list');
 const form = document.querySelector('.form');
 
-
-
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-
   const input = Object.fromEntries(
     new FormData(e.target)
-  )
+  );
   addTask(input.task);
   displayTasks();
-
-
 });
 
-
-
-
-// taskList.addEventListener('change', (e) => {
-//   e.preventDefault();
-//   alert('update');
-// });
-
-// modify below this line
-
-// modify above this line
 displayTasks();
--- src/modules/display.js
+++ src/modules/display.js
@@ -1,12 +1,9 @@
-export default function displayTasks(e) {
-
+export default function displayTasks() {
   const taskList = document.querySelector('.task-list');
   let parsedArr = JSON.parse(localStorage.getItem('taskArr')) || [];
-
   taskList.innerHTML = '';
   parsedArr.map((task) => {
     const taskItem = document.createElement('li');
-    // taskItem.classList.add('task-item', 'b-bottom', 'box');
     taskItem.innerHTML = `<form class="task-form b-bottom box">
     <input name="completed" type="checkbox" class="checkbox">
     <textarea name="description" rows="1" class="task-text full">${task.description}</textarea>
@@ -17,29 +14,20 @@ export default function displayTasks(e) {
     <img src="https://cdn-icons-png.flaticon.com/512/1828/1828743.png" alt="update-icon">
     </button>
     </form>`;
-    taskList.appendChild(taskItem);
-
-    
-    // try to split this into a function
+    taskList.appendChild(taskItem);   
     const deleteBtn = taskItem.querySelector('.delete');
     const updateBtn = taskItem.querySelector('.update');
     const updateText = taskItem.querySelector('.task-text');
-
     updateBtn.style.display = 'none';
-
     updateText.addEventListener('click', (e) => {
       e.preventDefault();
       updateBtn.style.display = 'block';
       deleteBtn.style.display = 'none';
       updateText.style.backgroundColor = '#f4f4f4';
-    
-      
     });
-
     const taskForm = taskItem.querySelector('.task-form');
     taskForm.addEventListener('submit', (e) => {
       e.preventDefault();
-
       const input = Object.fromEntries(
         new FormData(e.target)
       )
@@ -49,11 +37,8 @@ export default function displayTasks(e) {
       deleteBtn.style.display = 'block';
       updateText.style.backgroundColor = '#fff';
     });
-
-
     deleteBtn.addEventListener('click', (e) => {
       e.preventDefault();
-
       let temp = parsedArr.filter((item) => item !== task);
       parsedArr = temp;
       temp = parsedArr.map((item) => {
@@ -62,13 +47,8 @@ export default function displayTasks(e) {
       });
       parsedArr = temp;
       localStorage.setItem('taskArr', JSON.stringify(parsedArr));
-
       taskList.removeChild(taskItem);
     });
-    
-
-
-    // fix how to display the new task array
     return taskList;
   }); 
 }
