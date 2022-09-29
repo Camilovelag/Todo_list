c591e531960179fbb1938bf563ed9203542a04f4
Camilovelag <camilo.vela.galindo@gmail.com>
2022-09-29 12:55:58 -0500

    Updates

--- src/modules/clearAll.js
+++
@@ -1,5 +0,0 @@
-export default function clearAll() {
-  const parsedArr = JSON.parse(localStorage.getItem('taskArr'));
-  const temp = parsedArr.filter((item) => item.completed === false);
-  localStorage.setItem('taskArr', JSON.stringify(temp));
-}
